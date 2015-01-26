126f2584545e1dbb808d0e7244990aa7c3e41eaf
troy-halpin <thalpin@ufl.edu>
2015-01-26 14:39:26 -0500

    Create Text.java

---
+++ Text.java
@@ -0,0 +1,104 @@
+public class Text {
+
+	//There was so much text that for cleanliness' sake, I put it all into it's own class.
+	
+	Text() {
+	}
+	//definitely not the pokemon intro
+	public static String openingLine = ">Hello there! Welcome to the world of the Night's Watch.  My name is Jon Snow! People call me Lord Snow";
+	public static String line2 = ">This world is inhabited by dangerous creatures called White Walkers! For some people, White Walkers are myths. Others try to fight them. Myself... I try to prevent them from killing everybody as a profession.";
+	public static String nameLine = ">First, what is your name?";
+	public static String gotName = ">Right! So your name is ";
+	public static String introClosing = "! Your very own Westeros adventure is about to unfold! A world of dreams and adventures awaits! Let's go!";
+
+	//mission text
+	public static String goTalkToSnow = ", Jon was looking for you earlier. He needs to speak with you. He's waiting up ahead by the North Gate";
+	public static String jonGivingMission = "'Good morning, friend' says Jon. 'I have an urgent mission for you. I need you to go on a ranging to the Fist of The First Men. Unfortunately, due to our limited manpower here at Castle Black, you must go alone. We have heard reports of suspicious activities there, and that there may even be a White Walker. While this is highly unlikely, we need to investigate. Here, take this longsword and lantern to help you on your journey.";
+	public static String endGame = "'Welcome back Brother!' says Jon, smiling broadly. 'I knew you could do it! So, you've discovered the weakness of the White Walkers, eh? This could be what saves us all this coming Winter. We can now beat them! Thanks to you, the realm has a chance at survival.";
+	//general
+	public static String wrongInput = "Jon Snow's words echoed in your head: This isn't the time to use that!";
+	public static String gameOver = ">GAME OVER! Type quit to exit the game.";
+	public static String putInInventory = "You take it and put it in your inventory";
+	public static String alreadyHaveIt = "You already took it";
+	public static String superEffective = "It was super effective!";
+	public static String notVeryEffective = "It wasn't very effective";
+	public static String needToTalkToJon = "You need to talk to Jon to get that!";
+	public static String notNearJon = "He isn't close enough to talk to";
+	public static String jonSnowKnowsHowToKillYou = "Out of nowhere Jon Snow appears. His face is red, eyes are twitching. 'WHAT DID YOU SAY?!' he roars. 'I'LL SHOW YOU WHAT I KNOW!' and no one ever saw you again";
+
+	//events
+	public static String crastorGivingOldLetter = "Crastor glares at you. 'You want to know if I've heard anything about the Fist? The answer is no! But I did find this piece of paper on a dead crow I came across in the forest. If I give it to you will you go away?' He extends his hand, holding an oldletter.";
+	public static String bearKilledYou = ">Your attack was pretty much useless. The bear mauls you, and you are dead";
+	public static String killedBear = "You strike hard and fast. The bear was no match for you. Your stomach rumbles and you think of how good some bearmeat would be";
+	public static String cookBearMeat = "You put the bearmeat in the pot and cook it. Delicious smells waft up, and you take the cookedbearmeat and put it in your inventory";
+	public static String triedToRunFromBear = ">You try to flee but the bear was too fast. Don't worry though, you made a great meal for the bear.";
+	public static String caughtAfterTreason = ">As soon as the Brothers of the Night's Watch notice you, they restrain you and put you on trial for breaking your vows and murdering Crastor. You are found guilty and executed";
+	public static String triedToWalkInDarkCave = ">Out of the darkness a bear attacks you! You didn't have time to react, and the bear made short work of you.";
+	public static String itsAWight = "You look more closely at the body and see his name printed on his shield, 'Dwight'. You just have time to think to yourself 'What a strange name for this part of the realm' before Dwight starts to move. You then notice is his blue eyes and you realize Dwight's a WIGHT! There's no time to run away. You have no choice but to fight Dwight the wight!";
+	public static String dwightKilledYou = ">You attack Dwight with all of your might, but it merely causes a flesh wound. And Dwight doesn't care about flesh wounds. He shoots you several times with his bow, and you die";
+	public static String killedDwight = "Remembering wights' susceptibility to fire, you set Dwight alight. He will bother you no more.";
+	public static String triedToRunFromDwight = ">You turn to run, but you barely make it 5 feet before Dwight peppers you with arrows";
+	public static String jonKilledYou = ">You were fast, but Lord Snow was faster. Your head is now on an extended vacation from the rest of your body";
+	public static String killedCrastor = "You make short work of the old man. But you have betrayed your vows, and the next time anyone on the Night's Watch sees you, they will kill you on the spot";
+	public static String crastorKilledYou = ">Crastor deftly dodged your attack and dealt a dire blow with his dagger. It seems you are to be his next offering to the White Walkers";
+	public static String whiteWalkerAttacks = ">Suddenly the already low temperature drops steeply, and you notice all noise has stopped, save your breathing. It moved so quickly and quietly it was impossible to tell where it came from, but suddenly a White Walker appears in front of you. It's other-worldly gaze pierces your soul, and you are filled with terror and dread.";
+	public static String triedToRunFromWhiteWalker = ">Fight or Flight kicked in and you chose flight. Unfortunately you tripped in your haste to get the hell out of there, and the White Walker ran you through with it's icy blade. After death, your eyes turn icy blue and you join your new master";
+	public static String ateRawBeatMeat = ">Your stomach immediately feels weird, and you realize it might have been prudent to cook the meat first. Oh no! It appears you've caught Dysentery. You die a slowly, painfully, and full of shame as you evacuate your bowels to death";
+	public static String ateCookedBearMeat = "The cookedbearmeat is the best thing you've had in recent memory. You devour every last bite of it. Mmmmmmm tasty";
+	public static String whiteWalkerKilledYou = "You were too slow. The White Walker's sword felt felt cold as it pierced you. Your eyes turn to an icy blue, and you follow your new master";
+	public static String killedWhiteWalker = "You raise your dragonglass dagger and begin to fight. You make a feint and then attack as the White Walker attempts to block. You just barely cut it with your dagger, but a scratch is enough. The White Walker freezes and cracks, turning into dust. You should go back and tell Jon what happened";
+	//locations
+	public static String castleBlackDescription = "You are in Castle Black, headquarters of the Night's Watch";
+	public static String bySnow = "Jon Snow stands in front of you. He nods and greets you warmly";
+	public static String leftCastleBlack = "You are beyond the wall. The haunted forest lies ahead. The wall extends for miles to the east and west";
+	public static String desertionWarning = "You are deserting your mission! If you keep going south, your Brothers will kill you.";
+	public static String deserted = "You are dead. See? I told you they would kill you... Better luck next time, traitor.";
+	public static String alongWallWest = "The Wall towers next to you. It exudes cold from it's icy surface";
+	public static String alongWallEast = "The Wall towers next to you. It exudes cold from it's icy surface";
+	public static String kickedOutOfShadowkeep = "You are ranging, you aren't allowed in until your mission is complete. The Brothers have escorted you	back to the edge of the Haunted Forest.	Directly in front of you, a mighty river flows. The rest of the forest lies to the east and north";
+	public static String kickedOutOfEastwatch = "You are ranging, you aren't allowed in until your mission is complete. The Brothers have escorted you to their	north gate.	The path through the Haunted forest extends in front of you, and the wall runs to the west.";
+	public static String cantSwimInTheRiver = "You tried to swim in the river, then remembered you can't. The current grabs hold of you while your heavy armor traps you beneath the surface. Glub. Glub.";
+	public static String eastForestTrail = "You walk along the forest path. The trees here are ancient, gnarled and reaching for the little light they can get";
+	public static String cantGetThroughTheTrees = "You try to get through the trees, but they are too thick to pass. If only one of your pokemon knew cut. What? Uh, anyway, you dejectedly return to the center of the trail, all the while wondering what the hell a pokemon is and why you're thinking of it.";
+	public static String inTheHauntedForest = "You are in the haunted forest now. The trees grow dense and visibility is low.";
+	public static String forestClearingWithDeadDwight = "Dwight's body lies in the clearing. You're *pretty* sure he's dead for good this time";
+	public static String forestClearing = "You are in a small clearing. You notice a body lying in front of you. Something doesn't feel quite right.";
+	public static String crastorsKeepAlive = "You enter the 'keep'. Crastor looks up. 'What do you want, Crow?' he demands";
+	public static String timeToLeave = "You should probably go now";
+	public static String crastorsKeepDead = "Crastor's body is still on the floor where you left it. Still dead. The daughter-wives are long gone by now.";
+	public static String southCaveEntrance = "You enter the cave. The darkness is absolute. You wish you had a pokemon, that knew flash, but alas, this is the wrong sort of game now isn't it? But really this cave is incredibly dark. Scary dark. If only you had something to light it with.";
+	public static String caveWall = "Ouch! You ran into the wall of the cave. Be more careful next time!";
+	public static String lightedSouthCaveEntrance = "The light shows a path to the west and a path straight ahead";
+	public static String westCaveEntrance = "You enter the cave. The darkness is absolute. You wish you had a pokemon, that knew flash, but alas, this is the wrong sort of game now isn't it? But really this cave is incredibly dark. Scary dark. If only you had something to light it with.";
+	public static String lightedWestCaveEntrance = "The light shows a path to the east and a path to the north";
+	public static String northForestTrail = "The animal track follows a roughly east to west path. Snow is starting to fall and the temperature is dropping quickly.";
+	public static String forestsEdge = "The trees begin to thin and you can hear rushing water to the west and north. The terrain gets rockier and the footing is tricky";
+	public static String eastForestsEdgeEdge = "You try to get through the trees, but they are too thick to pass. If only one of your pokemon knew cut. What? Uh, anyway, you dejectedly return to the center of the trail, all the while wondering what the hell a pokemon is and why you're thinking of it.";
+	public static String hills = "The trees are all but gone. There are several inches of snow on the ground and the going is getting tougher. You are nearing your goal. You can see the fist rising up in the northwest.";
+	public static String fistOfTheFirstMen = "Dominating the skyline, the fist appears just as you've heard it described. A naturally defensible location, it provides a clear view of the surrounding area. A couple paces in front of you, something dark sticks out of the snow.";
+	public static String antlerRiver = "The antler river isn't deep, but the waters are freezing. After foolishly diving in, you catch hypothermia and die";
+	public static String inDarkCave = "You can't see a thing. What's that noise? Was that a growl?";
+	public static String caveIsLit = "Stalagtites and stalagmites litter the cave, am-i-stalag-right? In the northwest corner you see a bear. Unfortunately for you, it sees you too";
+	public static String caveNoBear = "The cave is filled with stalagtites and stalagmites, and thankfully not bears";
+	public static String fistWithDagger = "A naturally defensible location, the Fist provides a clear view of the surrounding area.";
+	//items in locations
+	public static String nearFireStarterAndPot = "Ahead of you, you see a dark shape in the snow. It's a pot! A firestarter lies inside";
+	public static String bowOnDwight = "As Dwight's corpse falls (hopefully for the last time), you notice he has a bow you could use...";
+	public static String byDragonGlassDagger = "A closer look shows what appears to be a dagger sticking out of the ground. It looks like it is made out of black glass. Could this be a dragonglass dagger?";
+	//items
+	public static String longswordDescription = "Made of heavy iron, the blade is sharp and dependable.";
+	public static String swungLongsword = "Your sword makes a whooshing sound as it slices through the air";
+	public static String shotBow = "The arrow whistles as it flies towards it's target";
+	public static String lanternDescription = "An old brass lantern, given to you by Jon for your journey";
+	public static String usedLantern = "You light your lantern. It is now much easier to see";
+	public static String oldLetter = "To whomever finds this, make haste! I have found the fatal weakness of The Others. The White Walkers can be stopped. They are weak to Dragon Glass weapons, and disintegrate on contact. I found some stored at The Fist. The wights they control are weak to fire.";
+	public static String usedFireStarter = "You strike the surface and sparks fly off. A small fire catches and starts to burn.";
+	public static String fireStarterDescription = "A dark gray chunk of metal, similar to a whetstone. It has a tiny stick you strike against it to create sparks";
+	public static String potDescription = "It's a pot! What did you think it was?";
+	public static String bowDescription = "A sturdy bow, a capable weapon in capable hands";
+	public static String bearMeatDescription = "It's raw, but it's meat. You might want to cook it before eating it";
+	public static String cookedBearMeatDescription = "The quintessential Beyond-The-Wall snack. The bear steak is well-marbled and as close to luxury as one can get out here";
+	public static String correctlyUsedPot = "You ignore the possible drug-related jokes and set your pot on the fire";
+	public static String dragonGlassDaggerDescription = "The weapon is made of a strange black substance the Valyrians called Dragon Glass. It used to be a spearhead, but it also serves as a dagger.";
+	public static String usedDragonGlassDagger = "You strike quickly with the dagger";
+}
