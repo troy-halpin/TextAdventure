90f8b1f663f6f2c04d8b9d522b6df82877fc33df
troy-halpin <thalpin@ufl.edu>
2015-01-26 14:44:32 -0500

    Delete Bow.java

--- Bow.java
+++
@@ -1,9 +0,0 @@
-public class Bow extends Item {
-	public static void use() {
-		GameMod.setUsedBow(true);
-		//Text.usedLantern;
-	}
-	public static String examine() {
-		return Text.bowDescription;
-	}
-}
